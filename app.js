e11fcf8e7d9b981c39c8d3e44dc608ad52b115fe
Justin Lock <jlock@TRHC.com>
2022-06-07 19:33:52 -0400

    using refs for getting an input value plus added lifecycle methods to understand the order and how they work in Vue

--- app.js
+++ app.js
@@ -10,12 +10,46 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
-    },
+      // this.message = this.currentUserInput;
+
+      // below is just another way of grabbing the value of an input by using refs
+      this.message = this.$refs.userText.value;
+      console.dir(this.$refs.userText);
+    }
   },
+  beforeCreate() {
+    console.log('beforeCreate()');
+    // here you could also send an http request to a server, add a timer, etc
+  },
+  created() {
+    console.log('created()');
+  },
+  beforeMount() {
+    console.log('beforeMount()');
+  },
+  mounted() {
+    // Here is where you actually see something on the screen from Vue. YOu can set breakpoints in the source tab to see
+    console.log('mounted()');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate()');
+  },
+  updated() {
+    // changes are now seen at this breakpoint after an update has occured
+    console.log('updated()');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount()');
+  },
+  unmounted() {
+    console.log('unmounted()');
+  }
 });
 
 app.mount('#app');
+setTimeout(function() {
+  app.unmount()
+}, 3000);
 
 // A little understanding of how Vue works and uses "Proxies" for a data object to update those properties when
 // certain keys are updated in that data object
