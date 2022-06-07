b5c40e47f1ca836b6199f27a7e43875d3101a0dc
Justin Lock <jlock@TRHC.com>
2022-06-06 22:46:55 -0400

    use vanilla js to create a data object, handler and proxy so you can see how a proxy works to update properties inside that data object thus becoming "reactive" like Vue

--- app.js
+++ app.js
@@ -16,3 +16,34 @@ const app = Vue.createApp({
 });
 
 app.mount('#app');
+
+// A little understanding of how Vue works and uses "Proxies" for a data object to update those properties when
+// certain keys are updated in that data object
+
+// data object to alter
+const data = {
+  message: 'Hello',
+  longMessage: 'Hello World!'
+};
+
+// handler with logic to use to pass into the proxy to update anything you want inside the data object
+const handler = {
+  set(target, key, value) {
+    console.log(target);
+    console.log(key);
+    console.log(value);
+    if (key === 'message') {
+      target.longMessage = value + 'World!'
+    }
+    target.message = value;
+  }
+};
+
+// create the proxy
+const proxy = new Proxy(data, handler);
+
+// when we update the message key, the handler logic will run thus when logging out long message below
+// it now becomes "Hello!!!!!! World!" instead of what we originally set it as => "Hello World!"
+proxy.message = 'Hello!!!!!!';
+
+console.log(proxy.longMessage)
